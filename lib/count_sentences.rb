80e0de6540a53552a328caca419f9b9353d36d6b
Zachary Goodman <znote89@gmail.com>
2021-08-10 21:44:30 -0400

    commit tree bf6e9549d721da3b32e9d31574cfcdcb1ad9e0f6

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).reject{|s| s.empty?}.count
   end
 end
